12f982ca03584cad78793b5990a877894af69d30
tilak <tilaka.krishna@philips.com>
2021-10-18 10:32:25 +0530

    Minor UI changes

--- app/src/main/java/com/tilak/apps/moviedb/common/AppConstants.kt
+++ app/src/main/java/com/tilak/apps/moviedb/common/AppConstants.kt
@@ -4,7 +4,7 @@ object AppConstants {
     const val DURATION_SPLASH = 1500L
 
     const val TAG: String = "movie_db"
-    const val BASE_URL = "http://api.themoviedb.org"
+    const val BASE_URL = "https://api.themoviedb.org"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w300"
     const val API_KEY = "3fa9058382669f72dcb18fb405b7a831"
 }
