90957bc5b473ae3201fd7b99e1752e054486bb49
tilak <tilakas13@gmail.com>
2021-10-23 19:32:25 +0530

    code refactor

--- app/src/main/java/com/tilak/apps/moviedb/common/AppConstants.kt
+++ app/src/main/java/com/tilak/apps/moviedb/common/AppConstants.kt
@@ -12,8 +12,8 @@ object AppConstants {
     const val DURATION_SPLASH = 1500L
 
     const val TAG: String = "movie_db"
-    const val BASE_URL = "https://api.themoviedb.org"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val LIST_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w200"
-    const val BANNER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+    const val BANNER_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w300"
     const val API_KEY = "3fa9058382669f72dcb18fb405b7a831"
 }
--- app/src/main/java/com/tilak/apps/moviedb/data/network/ApiService.kt
+++ app/src/main/java/com/tilak/apps/moviedb/data/network/ApiService.kt
@@ -17,13 +17,13 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("/3/discover/movie?sort_by=popularity.desc")
+    @GET("discover/movie?sort_by=popularity.desc")
     suspend fun getPopularMovies(@Query("page") page: Int): MovieListModel
 
 
-    @GET("3/movie/{movie_id}")
+    @GET("movie/{movie_id}")
     suspend fun getMovieDetail(@Path("movie_id") movie_id: Int): MovieDetail
 
-    @GET("3/movie/{movie_id}/credits")
+    @GET("movie/{movie_id}/credits")
     suspend fun getCastCewDetails(@Path("movie_id") movie_id: Int): CastCrewModel
 }
