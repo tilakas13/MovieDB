cea5fd3fb7b5e81a73ea2d401b7d5f1afaa768cb
tilak <tilakas13@gmail.com>
2021-11-22 21:37:22 +0530

    Lint fixes

--- app/src/main/java/com/tilak/apps/moviedb/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/tilak/apps/moviedb/ui/splash/SplashViewModel.kt
@@ -11,9 +11,9 @@ package com.tilak.apps.moviedb.ui.splash
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tilak.apps.moviedb.common.AppConstants
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,7 +26,7 @@ class SplashViewModel   @Inject constructor() : ViewModel() {
     private val mutableLiveData = MutableLiveData<SplashState>()
 
     init {
-        GlobalScope.launch {
+        viewModelScope.launch {
             delay(AppConstants.DURATION_SPLASH)
             mutableLiveData.postValue(SplashState.splashScreen)
         }
